b4641037a559c4b2bb08509af90f640763ebb32d
1exz <alex.buchko.m@gmail.com>
2022-07-13 12:28:13 +0300

    fit:done task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr=[];
+  if (matrix === undefined) {
+    return arr=[]
+  }
+  for (i=0;i<matrix.length;i++){ 
+     if (i==0){
+      arr[0]=matrix[0]
+    }
+     else if(i%2==0){
+      arr[i]=matrix[i]
+    }
+     else{
+      arr[i]=matrix[i].reverse()
+    }
+  }
+  return arr.flat(Infinity)
 }
+
